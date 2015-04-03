1a05699ea512f398cf1ade3b35d6722cb834c27d
CostaxCoffee <u12104592@tuks.co.za>
2015-04-03 19:20:49 +0200

    Started integration of CSDS and Authorization

--- Modules/CSDS.js
+++ Modules/CSDS.js
@@ -78,6 +78,102 @@ module.exports.CheckCon= function Check(client,LoginResult)
     });
 }
 
+module.exports.getUsersRolesForModule= function getUsersRolesForModule(getUsersRolesForModuleRequest,client,getUsersRolesForModuleResult) {
+    {
+        base = 'ou=Computer Science,o=University of Pretoria,c=ZA';
+        var opts =
+        {
+            filter: "memberuid=" + getUsersRolesForModuleRequest.mID(),//+loginRequest.username();
+            scope: 'sub'
+        };
+
+        var entry=new Array();
+        var assert = require("assert");
+        return client.search(base, opts, function (err, res) {
+            if (err)
+            {
+                client.unbind();
+                return getUsersRolesForModuleResult(err, null,getUsersRolesForModuleRequest.mID(),getUsersRolesForModuleRequest.uID());
+            }
+            res.on('searchEntry', function (_entry)
+            {
+                entry.push(_entry);
+            });
+
+            res.on('error', function (err) {
+                client.unbind();
+                return getUsersRolesForModuleResult(err, null,getUsersRolesForModuleRequest.mID(),getUsersRolesForModuleRequest.uID());
+            });
+
+            res.on('end', function () {
+                if (!entry) {
+                    client.unbind();
+                    return getUsersRolesForModuleResult(new Error(getUsersRolesForModuleRequest.muid() + ' not found'), null,getUsersRolesForModuleRequest.mID(),getUsersRolesForModuleRequest.uID());
+
+                }
+                return client.bind(entry.dn.toString(), LoginRequest.password(), function (err) {
+                    if (err) {
+                        client.unbind();
+                        return getUsersRolesForModuleResult(err, null,getUsersRolesForModuleRequest.mID(),getUsersRolesForModuleRequest.uID());
+                    }
+                    return client.unbind(function (err) {
+                        assert.ifError(err);
+                        return getUsersRolesForModuleResult(null, entry.toObject(),getUsersRolesForModuleRequest.muid());
+                    });
+                });
+            });
+        });
+    }
+}
+
+module.exports. getUsersWithRole= function  getUsersWithRole( getUsersWithRoleRequest,client, getUsersWithRoleResult)
+{
+    {
+        base = 'ou=Computer Science,o=University of Pretoria,c=ZA';
+        var opts =
+        {
+            filter: "cn=" +  getUsersWithRoleRequest.roleID(),//+loginRequest.username();
+            scope: 'sub'
+        };
+
+        var entry=new Array();
+        var assert = require("assert");
+        return client.search(base, opts, function (err, res) {
+            if (err)
+            {
+                client.unbind();
+                return  getUsersWithRoleResult(err, null,null);
+            }
+            res.on('searchEntry', function (_entry)
+            {
+                entry.push(_entry);
+            });
+
+            res.on('error', function (err) {
+                client.unbind();
+                return  getUsersWithRoleResult(err, null,null);
+            });
+
+            res.on('end', function () {
+                if (!entry) {
+                    client.unbind();
+                    return  getUsersWithRoleResult(new Error( getUsersWithRoleRequest.muid() + ' not found'), null);
+                }
+                return client.bind(entry.dn.toString(), LoginRequest.password(), function (err) {
+                    if (err) {
+                        client.unbind();
+                        return  getUsersWithRoleResult(err,null,null);
+                    }
+                    return client.unbind(function (err) {
+                        assert.ifError(err);
+                        return  getUsersWithRoleResult(null, entry.toObject(), getUsersWithRoleRequest.roleid());
+                    });
+                });
+            });
+        });
+    }
+}
+
 module.exports.GetUserInfo= function UserInfo(client,UserInfoRequest,UserInfoResult)
 {
     base='ou=Computer Science,o=University of Pretoria,c=ZA';
