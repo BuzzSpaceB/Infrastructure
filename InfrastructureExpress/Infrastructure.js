1da62d6a854a7b0bb17c93f9ada3705a27c5e3a5
WMostert1 <werner.mostert1@gmail.com>
2015-04-06 20:33:15 +0200

    Changed space aop

--- InfrastructureExpress/Infrastructure.js
+++ InfrastructureExpress/Infrastructure.js
@@ -7,9 +7,6 @@ var aop = require("node-aop");
 
 var logged_in_id = "";
 
-
-
-
 //REQUIRED FUNCTIONS
 function login(username, password) {
     logged_in_id = space.login(username, password, CSDS);
@@ -44,23 +41,83 @@ var getAuthorizationBeforeIntercept = aop.before(authorization, "getAuthorizatio
  * START OF CODE THAT EXTENDS SPACES MODULE
  *
  */
-var addAdminBeforeIntercept = aop.before(space, "addAdministrator", function (_module_id, _user_id) {
-    //TODO-Werner re-add your code(It's at the end of this document)
-});
 
-var removeAdminBeforeIntercept = aop.before(space, "removeAdministrator", function (_module_id, _user_id) {
-    //TODO-Werner re-add your code(It's at the end of this document)
-});
 
-var createBuzzSpaceBeforeIntercept = aop.before(space, "createBuzzSpace", function (module_ID, user_ID, academic_Year) {
-//    TODO-Werner re-add your code(It's at the end of this document)
-});
+function addAdministrator(module_id, user_id) {
+        if (user_id == logged_in_id) {
+            console.log("You are already an administrator");
+            return false;
+        }
 
-var closeBuzzSpaceBeforeIntercept = aop.before(space, "closeBuzzSpace", function (module_ID) {
-    //TODO-Werner  re-add your code(It's at the end of this document)
-    //TODO-Trevor remove all restrictions for buzzfeed
-    //TODO-Andreas remove all notification for buzzfeed
-});
+        if (space.isAdministrator(user_id)) {
+            console.log("That user is already an administrator");
+            return false;
+        }
+
+        space.addAdministrator(module_id, user_id);
+        console.log("Successfully added administrator");
+        return true;
+}
+
+
+function removeAdministrator(module_id, user_id) {
+        space.removeAdministrator(module_id, user_id);
+        console.log("Successfully removed administrator");
+        return true;
+}
+
+var closeBuzzSpaceBeforeIntercept;
+function closeBuzzSpace(moduleID) {
+    try {
+        if (closeBuzzSpaceBeforeIntercept == undefined) {
+            closeBuzzSpaceBeforeIntercept = aop.before(space, "closeBuzzSpace", function (module_ID) {
+                //TODO: Not 100 as status points - calculate
+                if (!authorization.isAuthorized(module_ID, "space", "closeBuzzSpace", logged_in_id, 100)) {
+                    throw "Not authorized to Close Buzz Space"
+                }
+            });
+        }
+
+        space.closeBuzzSpace(moduleID);
+        console.log("Buzz Space successfully closed");
+        return true;
+    } catch (NotAuthorizedException) {
+        console.log(NotAuthorizedException);
+        return false;
+    }
+}
+
+
+function createBuzzSpace(moduleID) {
+    var academicYear = new Date().getFullYear();
+        space.createBuzzSpace(moduleID, logged_in_id, academicYear);
+        console.log("Buzz Space successfully created");
+
+        return true;
+
+    }
+
+
+
+
+
+//var addAdminBeforeIntercept = aop.before(space, "addAdministrator", function (_module_id, _user_id) {
+//    //TODO-Werner re-add your code(It's at the end of this document)
+//});
+//
+//var removeAdminBeforeIntercept = aop.before(space, "removeAdministrator", function (_module_id, _user_id) {
+//    //TODO-Werner re-add your code(It's at the end of this document)
+//});
+//
+//var createBuzzSpaceBeforeIntercept = aop.before(space, "createBuzzSpace", function (module_ID, user_ID, academic_Year) {
+////    TODO-Werner re-add your code(It's at the end of this document)
+//});
+//
+//var closeBuzzSpaceBeforeIntercept = aop.before(space, "closeBuzzSpace", function (module_ID) {
+//    //TODO-Werner  re-add your code(It's at the end of this document)
+//    //TODO-Trevor remove all restrictions for buzzfeed
+//    //TODO-Andreas remove all notification for buzzfeed
+//});
 /*
  * END OF CODE THAT EXTENDS SPACES MODULE
  */
@@ -83,111 +140,16 @@ var closeBuzzSpaceBeforeIntercept = aop.before(space, "closeBuzzSpace", function
 */
 
  module.exports.space = space;
+    module.exports.login = login;
+    module.exports.logout = logout;
+    module.exports.closeBuzzSpace = closeBuzzSpace;
+    module.exports.createBuzzSpace = createBuzzSpace;
+    module.exports.addAdministrator = addAdministrator;
+    module.exports.removeAdministrator = removeAdministrator;
+
+
+
+
  module.exports.authorization = authorization;
  module.exports.csds = csds;
  module.exports.notification = notification;
-
-/*
- OLD CODE:
- var addAdminBeforeIntercept;
- function addAdministrator(module_id, user_id) {
- try {
- if (user_id == logged_in_id) {
- console.log("You are already an administrator");
- return false;
- }
-
- if (space.isAdministrator(user_id)) {
- console.log("That user is already an administrator");
- return false;
- }
-
- if (addAdminBeforeIntercept == undefined) {
- addAdminBeforeIntercept = aop.before(space, "addAdministrator", function (_module_id, _user_id) {
- //TODO: Not 100 as status points - calculate
- //TODO: Remove authorization
- if (!authorization.isAuthorized(module_id, "space", "addAdministrator", logged_in_id, 100)) {
- throw "Not authorized to add Administrator";
- }
- });
- }
-
- space.addAdministrator(module_id, user_id);
- console.log("Successfully added administrator");
- return true;
-
- } catch (NotAuthorizedException) {
- console.log(NotAuthorizedException);
- return false;
- }
- }
-
- var removeAdminBeforeIntercept;
- function removeAdministrator(module_id, user_id) {
-
- try {
- if (removeAdminBeforeIntercept == undefined) {
- removeAdminBeforeIntercept = aop.before(space, "removeAdministrator", function (_module_id, _user_id) {
- //TODO: Not 100 as status points - calculate
- //TODO: remove authorization
- if (!authorization.isAuthorized(module_id, "space", "removeAdministrator", logged_in_id, 100)) {
- throw "Not authorized to remove Administrator";
- }
- });
- }
-
- space.removeAdministrator(module_id, user_id);
- console.log("Successfully removed administrator");
- return true;
-
- } catch (NotAuthorizedException) {
- console.log(NotAuthorizedException);
- return false;
- }
- }
-
- var closeBuzzSpaceBeforeIntercept;
- function closeBuzzSpace(moduleID) {
- try {
- if (closeBuzzSpaceBeforeIntercept == undefined) {
- closeBuzzSpaceBeforeIntercept = aop.before(space, "closeBuzzSpace", function (module_ID) {
- //TODO: Not 100 as status points - calculate
- if (!authorization.isAuthorized(module_ID, "space", "closeBuzzSpace", logged_in_id, 100)) {
- throw "Not authorized to Close Buzz Space"
- }
- });
- }
-
- space.closeBuzzSpace(moduleID);
- console.log("Buzz Space successfully closed");
- return true;
- } catch (NotAuthorizedException) {
- console.log(NotAuthorizedException);
- return false;
- }
- }
-
- var createBeforeIntercept;
- function createBuzzSpace(moduleID) {
- var academicYear = new Date().getFullYear();
-
- try {
-
- if (createBeforeIntercept == undefined) {
- createBeforeIntercept = aop.before(space, "createBuzzSpace", function (module_ID, user_ID, academic_Year) {
- //TODO: Not 100 as status points - calculate
- //TODO remove authorization
- if (!authorization.isAuthorized(module_ID, "space", "createBuzzSpace", logged_in_id, 100)) {
- throw "Not authorized to Create Buzz Space"
- }
- });
- }
- space.createBuzzSpace(moduleID, logged_in_id, academicYear);
- console.log("Buzz Space successfully created");
-
- return true;
- } catch (NotAuthorizedException) {
- console.log(NotAuthorizedException);
- return false;
- }
- }*/
