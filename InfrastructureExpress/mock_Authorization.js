9a556025b4cb26a50cdf3d99371b163d70f8236d
Trevor Austin <taustin.swim@gmail.com>
2015-04-06 21:54:36 +0200

    updated MockAuthorization

--- InfrastructureExpress/mock_Authorization.js
+++ InfrastructureExpress/mock_Authorization.js
@@ -1,45 +1,62 @@
 /**
  * Created by Trevor on 2015-04-05.
  */
-function addAuthorization(buzzspaceID, statusPoints, Role, objectName, objectMethod) {
-    console.log("Buzzspace: " + buzzspaceID + " has added restriction on service: " + objectName + "." + objectMethod + " with current restrictions: ");
-    console.log("Role: " + Role);
+function addAuthorization(buzzspaceName, statusPoints, role, objectName, objectMethod) {
+    if (buzzspaceName == "COS301" && role == "student" && objectName == "thread" && objectMethod == "edit") {
+        if (statusPoints == 100) {
+            throw "Restriction already exists"
+        } else {
+            throw "Restriction already exists with different status points value"
+        }
+    }
+    console.log("Buzzspace: " + buzzspaceName + " has added restriction on service: " + objectName + "." + objectMethod + " with current restrictions: ");
+    console.log("Role: " + role);
     console.log("Status Points: " + statusPoints);
-    return true;
 }
 
-function removeAuthorization(authorizedID) {
-    console.log("Removing authorizationID: " + authorizedID);
-    return true;
+function removeAuthorization(buzzspaceName, objectName, objectMethod) {
+    if (buzzspaceName != "COS301" && objectName != "thread" && objectMethod != "edit") {
+        throw "No restriction exists for current remove";
+    }
+    console.log("Removing restriction on " + objectName + "." + objectMethod + "in buzzspace: " + buzzspaceName);
 }
 
-function isAuthorized(moduleID, objectName, objectMethod, userID, statusPoints) {
-    console.log("Checking if userID: " + userID + " has access to " + objectName + "." + objectMethod + " in module: " + moduleID);
+function isAuthorized(buzzspaceName, objectName, objectMethod, userID, statusPoints) {
+    console.log("Checking if userID: " + userID + " has access to " + objectName + "." + objectMethod + " in module: " + buzzspaceName);
 
-    if(userID =="" || userID==undefined){
-        console.log("Access is not authorized");
-        return false;
+    if (userID == "" || userID == undefined) {
+        throw "Student is not authorized";
     }
     console.log("Access is authorized");
-    return true;
 }
 
-function getAuthorized(buzzSpaceID) {
-    console.log("Getting restrictions in place from module: " + buzzSpaceID);
-    return null;
+function getAuthorized(buzzSpaceName) {
+    console.log("Getting restrictions in place from module: " + buzzSpaceName);
+    var arr = [{
+        objectName: "thread",
+        objectType: "edit",
+        role: "student",
+        statusPoints: 100
+    }, {
+        objectName: "thread",
+        objectType: "edit",
+        role: "student",
+        statusPoints: 100
+    }];
+    return arr;
 }
 
-function updateAuthorized(buzzspaceID, statusPoints, role, objectName, objectMethod) {
-    console.log("Updating restriction : " + objectName + "." + objectMethod + "in buzzspace: " + buzzspaceID + " to:");
+function updateAuthorized(buzzspaceName, statusPoints, role, objectName, objectMethod, newRole, newStatusPoints) {
+    if (!(buzzspaceName == "COS301" && role == "student" && objectName == "thread" && objectMethod == "edit")){
+        throw "Current restriction cannot be updated as it cannot be found"
+    }
+    console.log("Updating restriction : " + objectName + "." + objectMethod + "in buzzspace: " + buzzspaceName + " to:");
     if (role != null) {
         console.log("Role: " + role);
     }
-
     if (statusPoints != null)
         console.log("Status Points: " + statusPoints);
-    return true;
 }
-
 
 
 exports.isAuthorized = isAuthorized;
